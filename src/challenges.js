fafa6659ea29d48099e0e5e73707ff180aba78f1
Felipe Gallassi <felipegallassi@gmail.com>
2021-12-02 15:44:47 -0300

    requisitos 1 e 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,17 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(p1, p2) {
+  if (p1 === true && p2 === true) {
+    return true; 
+  }
+  else {
+    return false;
+  }
 }
 
+
 // Desafio 2
-function calcArea() {
-  // seu código aqui
+function calcArea(base, height) {
+  return (base * height)/2
 }
 
 // Desafio 3
